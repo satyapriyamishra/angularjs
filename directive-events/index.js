4ff7bbeb6ad6e6400dbdb4a7d921f3a6722f1357
Satyapriya <satyapriyamishra111.com>
2018-01-27 00:51:35 +0530

    event addition

--- directive-events/index.js
+++ directive-events/index.js
@@ -7,6 +7,16 @@ app.directive('frontend', function () {
         });
     }
 });
+app.directive('angularjs', function () {
+    return function (scope, element) {
+        element.bind('mouseenter', function () {
+            element[0].style.border = '1px solid red';
+        });
+        element.bind('mouseleave', function () {
+            element[0].style.border = '';
+        });
+    }
+});
 app.controller('myctrl1', function ($scope) {
 
 })
