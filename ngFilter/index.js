c7a5c770e5c2da62061f5e195ef40ec72b6f52ec
Satyapriya <satyapriyamishra111.com>
2018-01-26 23:22:56 +0530

    ngfilter

---
+++ ngFilter/index.js
@@ -0,0 +1,19 @@
+var app = angular.module('app', []);
+app.factory ('Data', function () {
+    var name =  [
+        'Jani',
+        'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+    ];
+    return name;
+})
+app.controller ('myctrl1', function ($scope,Data) {
+    $scope.name = Data;
+    
+})
